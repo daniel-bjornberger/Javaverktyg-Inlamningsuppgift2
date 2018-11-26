336b39b2fe59ebd6f527381e6a8861346b1fdc97
Daniel Björnberger <daniel.bjornberger@gmail.com>
2018-11-26 08:34:33 +0000

    Update GuessingGame, handle arguments + exceptions

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,9 +2,25 @@ public class GuessingGame {
 
     public static void main(String[] args) {
 
-        Guesser guesser = new Guesser(0, 1000);
+        try {
 
-        guesser.start();
+            int args0 = Integer.parseInt(args[0]);
+            int args1 = Integer.parseInt(args[1]);
+
+            Guesser guesser = new Guesser(args0, args1);
+            guesser.start();
+
+        }
+        catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+
+            System.err.println("Invalid arguments, or too few arguments. The arguments shall be two integers. The program will now terminate.");
+
+        }
+        catch (IllegalArgumentException e) {
+
+            System.err.println("Invalid arguments. The first integer shall be lower than the second integer. The program will now terminate.");
+
+        }
 
     }
 
